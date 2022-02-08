2041512c0ba11ba005b9a12ec2642611831d7b55
Ben Shukrallah <benjamin.shukrallah@live.longwood.edu>
2022-02-07 22:56:13 -0500

    Updated application.cpp with OpenGL Buffer

--- OpenGL/src/Application.cpp
+++ OpenGL/src/Application.cpp
@@ -26,22 +26,38 @@ int main(void)
         return -1;
     }
 
+    float positions[6] = {
+       -0.5f, -0.5f,
+        0.0f,  0.5f,
+        0.5f,  0.0f
+
+    };
+
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
     glewInit();
     LOG("Debug Logging is True");
     LOG(glGetString(GL_VERSION));
+    //Create a buffer
+    unsigned int buffer;
+    //We need 1 buffer, and assign a pointer to the unsigned int above
+    glGenBuffers(1, &buffer);
+    //Tell opengl what to do with the buffer
+    glBindBuffer(GL_ARRAY_BUFFER, buffer);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(positions), positions, GL_STATIC_DRAW);
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0);
+
+
 
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
-        glBegin(GL_TRIANGLES);
-        glVertex2f(-0.5f, -0.5f);
-        glVertex2f(0.0f, 0.5f);
-        glVertex2f(0.5f, 0.0f);
-        glEnd();
+
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
 
